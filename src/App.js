60557132c14b06666adf57749d25d2faf5ebf7f8
Cec-Adc <cecile.adach@gmail.com>
2021-07-30 14:30:59 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="Weather App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/Cec-Adc/react-weather-app-final"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced project
-        </a>{" "}
-        by Cécile
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/Cec-Adc/react-weather-app-final"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced project
+          </a>{" "}
+          by Cécile
+        </footer>
+      </div>
     </div>
   );
 }

fd4826bb804cfdd491e194185385e6b7b94557b8
Cec-Adc <cecile.adach@gmail.com>
2021-07-30 11:30:13 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="Weather App">
       <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/Cec-Adc/react-weather-app-final"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced project
+        </a>{" "}
+        by Cécile
+      </footer>
     </div>
   );
 }

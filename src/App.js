7817f56bd1a397a93025ef6636743b5380611a8c
Cec-Adc <cecile.adach@gmail.com>
2022-06-12 18:04:23 +0200

    Default city changed to Mey

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="Weather App">
       <div className="container">
-        <Weather defaultCity="Hamburg" />
+        <Weather defaultCity="Mey" />
       </div>
       <footer className="mt-4">
         <a
